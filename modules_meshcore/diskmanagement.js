62e2d8be39a3e166ae30f97c4391c0e994ac055d
johnglavan71 <78128393+johnglavan71@users.noreply.github.com>
2026-06-24 00:44:55 -0500

    Add files via upload

--- modules_meshcore/diskmanagement.js
+++ modules_meshcore/diskmanagement.js
@@ -26,42 +26,47 @@ function consoleaction(args, rights, sessionid, parent) {
     switch (fnname) {
         case 'getdisks': {
             var fileRand = Math.random().toString(32).replace('0.', '');
-            var fileName = 'dmout' + fileRand + '.txt';
+            var pName = 'dmscript' + fileRand + '.ps1';
+            var oName = 'dmout' + fileRand + '.txt';
             
             var psScript = "$disks = Get-Disk | Select-Object Number, PartitionStyle, TotalSize, OperationalStatus; " +
                            "$partitions = Get-Partition | Select-Object DiskNumber, PartitionNumber, DriveLetter, Type, Size; " +
                            "$volumes = Get-Volume | Select-Object DriveLetter, FileSystemLabel, FileSystem, HealthStatus, Size, SizeRemaining; " +
-                           "@{ disks = @($disks); partitions = @($partitions); volumes = @($volumes) } | ConvertTo-Json -Depth 4 -Compress | Out-File " + fileName + " -Encoding UTF8";
+                           "@{ disks = @($disks); partitions = @($partitions); volumes = @($volumes) } | ConvertTo-Json -Depth 4 -Compress | Out-File " + oName + " -Encoding UTF8";
             
-            var child = require('child_process').execFile(process.env['windir'] + '\\system32\\WindowsPowerShell\\v1.0\\powershell.exe', ['-NoLogo', '-NoProfile', '-NonInteractive']);
+            var fs = require('fs');
+            try { fs.writeFileSync(pName, psScript); } catch(e) {}
+
+            var child = require('child_process').execFile(process.env['windir'] + '\\system32\\WindowsPowerShell\\v1.0\\powershell.exe', ['-NoLogo', '-NoProfile', '-ExecutionPolicy', 'Bypass']);
             var errstr = '';
             child.stderr.on('data', function (chunk) { errstr += chunk; });
-            child.stdin.write(psScript + '\r\n');
+            child.stdin.write('.\\' + pName + '\r\n');
             child.stdin.write('exit\r\n');
             
             child.on('exit', function (code) {
                 try {
-                    var fs = require('fs');
-                    var out = fs.readFileSync(fileName, 'utf8').toString();
+                    var out = fs.readFileSync(oName, 'utf8').toString();
                     if (out.charCodeAt(0) === 0xFEFF) out = out.substring(1);
                     if (out) {
                         try { out = out.trim(); } catch (e) { }
                         var response = { type: 'disks', data: JSON.parse(out) };
                         if (isWsconnection) {
-                            wscon.write(new Buffer(JSON.stringify(response)));
+                            wscon.write(Buffer.from(JSON.stringify(response)));
                         } else {
                             mesh.SendCommand({ "action": "msg", "type": "console", "value": JSON.stringify(response), "sessionid": sessionid });
                         }
                     } else {
                         if (isWsconnection) {
-                            wscon.write(new Buffer(JSON.stringify({type: 'error', message: 'No disk data returned by PowerShell. Stderr: ' + errstr})));
+                            wscon.write(Buffer.from(JSON.stringify({type: 'error', message: 'No disk data returned by PowerShell. Stderr: ' + errstr})));
                         }
                     }
-                    try { fs.unlinkSync(fileName); } catch (e) {}
+                    try { fs.unlinkSync(oName); } catch (e) {}
+                    try { fs.unlinkSync(pName); } catch (e) {}
                 } catch (e) {
                     if (isWsconnection) {
-                        wscon.write(new Buffer(JSON.stringify({type: 'error', message: e.toString() + ' Stderr: ' + errstr})));
+                        wscon.write(Buffer.from(JSON.stringify({type: 'error', message: e.toString() + ' Stderr: ' + errstr})));
                     }
+                    try { fs.unlinkSync(pName); } catch (e) {}
                 }
             });
             break;
